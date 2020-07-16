105ac0f4ca40e8ec2c848ffe33e3abfcff7f4ada
AndrewGronin <you@example.com>
2020-07-16 15:13:15 +0400

    fixed name and incorrect link in product test

--- test_product_page.py
+++ test_product_page.py
@@ -84,8 +84,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 
 @pytest.mark.need_review
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
@@ -116,7 +116,7 @@ class TestUserAddToBasketFromProductPage():
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         page = ProductPage(browser, link)
         page.open()
         page.add_to_basket()
